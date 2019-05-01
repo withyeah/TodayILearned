1fa68d48b83667a46788482e8bbeb49d682b1d8a
withyeah <areygim@gmail.com>
2019-05-01 19:58:04 +0900

    20190501 | javascript

--- Javascript/js/array_helper_method.js
+++ Javascript/js/array_helper_method.js
@@ -1,55 +1,55 @@
-// // 1. forEach
-// // forEach 함수는 아무것도 return 하지 않는다.
+// 1. forEach
+// forEach 함수는 아무것도 return 하지 않는다.
 
-// // ES5
-// var colors = ['red', 'blue', 'green',]
-// for (var i = 0; i < colors.length; i++) {
-//     console.log(colors[i])
-// }
-// // ES6
-// const COLORS = ['red', 'blue', 'green',]
-// COLORS.forEach(function (color) {
-//     console.log(color)
-// })
-// // arrow function
-// COLORS.forEach(color => console.log(color))
+// ES5
+var colors = ['red', 'blue', 'green',]
+for (var i = 0; i < colors.length; i++) {
+    console.log(colors[i])
+}
+// ES6
+const COLORS = ['red', 'blue', 'green',]
+COLORS.forEach(function (color) {
+    console.log(color)
+})
+// arrow function
+COLORS.forEach(color => console.log(color))
 
 
-// // exercise 1-1] 아래 함수의 for를 forEach로 바꾸시오
-// function handlePosts() {
-//     const posts = [
-//         { id: 23, title: 'daily news' },
-//         { id: 52, title: 'Code City' },
-//         { id: 105, title: 'The Ruby' },
-//     ]
-//     for (let i = 0; i < posts.length; i++) {
-//         // 어쩌구
-//     }
-// }
-// // 답
-// function handlePosts() {
-//     const posts = [
-//         { id: 23, title: 'daily news' },
-//         { id: 52, title: 'Code City' },
-//         { id: 105, title: 'The Ruby' },
-//     ]
-//     posts.forEach(post => console.log(post))
-// }
+// exercise 1-1] 아래 함수의 for를 forEach로 바꾸시오
+function handlePosts() {
+    const posts = [
+        { id: 23, title: 'daily news' },
+        { id: 52, title: 'Code City' },
+        { id: 105, title: 'The Ruby' },
+    ]
+    for (let i = 0; i < posts.length; i++) {
+        // 어쩌구
+    }
+}
+// 답
+function handlePosts() {
+    const posts = [
+        { id: 23, title: 'daily news' },
+        { id: 52, title: 'Code City' },
+        { id: 105, title: 'The Ruby' },
+    ]
+    posts.forEach(post => console.log(post))
+}
 
-// // exercise 1-2] 아래 코드의 images 배열안에 있는 정보(height, width)를 곱해 넓이를 구하여
-// // areas 배열에 저장하는 코드를 forEach 헬퍼를 사용해 작성해보자
-// const images = [
-//     { height: 10, width: 30 },
-//     { height: 20, width: 90 },
-//     { height: 54, width: 32 },
-// ]
-// const areas = []
-// images.forEach(function (image) {
-//     areas.push(image.width*image.height)
-// })
-// console.log(areas)
-// // arrow function
-// images.forEach(image => areas.push(image.width*image.height))
+// exercise 1-2] 아래 코드의 images 배열안에 있는 정보(height, width)를 곱해 넓이를 구하여
+// areas 배열에 저장하는 코드를 forEach 헬퍼를 사용해 작성해보자
+const images = [
+    { height: 10, width: 30 },
+    { height: 20, width: 90 },
+    { height: 54, width: 32 },
+]
+const areas = []
+images.forEach(function (image) {
+    areas.push(image.width*image.height)
+})
+console.log(areas)
+// arrow function
+images.forEach(image => areas.push(image.width*image.height))
 
 
 
