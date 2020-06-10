7599e20a33dc829c1cba268251e632db419fc700
Alex Gim <areygim@gmail.com>
2020-06-10 14:31:28 +0900

    20200609 | I Am Poor app udemy section 3

---
+++ iOS_Swift/app_I_Am_Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by 김예랑 on 2020/06/09.
+//  Copyright © 2020 김예랑. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
