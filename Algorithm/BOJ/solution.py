6d131eca3435bc9adf37cb2e706b364e625820d1
Alex Gim <areygim@gmail.com>
2020-08-25 11:24:36 +0900

    20200824 | ios udemy section 13 closures challenge

---
+++ Algorithm/BOJ/solution.py
@@ -0,0 +1,11 @@
+result = int(input())
+if result >= 90:
+    print("A")
+elif result >= 80:
+    print("B")
+elif result >= 70:
+    print("C")
+elif result >= 60:
+    print("D")
+else:
+    print("F")
---
+++ iOS_Swift/Closures_Challenge/Closures_Challenge/main.swift
@@ -0,0 +1,17 @@
+//
+//  main.swift
+//  Closures_Challenge
+//
+//  Created by 김예랑 on 2020/08/23.
+//  Copyright © 2020 김예랑. All rights reserved.
+//
+
+import Foundation
+
+let array = [1, 2, 3, 4, 5]
+
+let newArray = array.map({"\($0)"})
+
+print(newArray)
+
+
