00aa7637597ed5700e57ac1a02ff1d7177f35e35
Alex Gim <areygim@gmail.com>
2020-09-08 18:06:09 +0900

    200908 | Flash Chat iOS app udemy section 15

---
+++ iOS_Swift/app12_Flash_Chat/Flash Chat iOS13/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Flash Chat iOS13
+//
+//  Created by 김예랑 on 2020/09/03.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+struct K {
+    static let registerSegue = "RegisterToChat"
+    static let loginSegue = "LoginToChat"
+    
+}
---
+++ iOS_Swift/app12_Flash_Chat/Flash Chat iOS13/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by 김예랑 on 2020/09/04.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String // email address of the sender
+    let body: String
+}
