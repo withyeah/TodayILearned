46b53e970c7119c561addf5c5dfd21173d4d43ba
Alex Gim <areygim@gmail.com>
2020-10-07 10:11:35 +0900

    iOS CoreDataTest udemy section 19

---
+++ iOS_Swift/CoreDataTest/CoreDataTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDataTest
+//
+//  Created by 김예랑 on 2020/10/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
