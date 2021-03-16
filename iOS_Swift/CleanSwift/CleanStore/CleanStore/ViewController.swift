9ea1294b2765da82b7b1b3b74c2a9ad68646f083
Alex Gim <areygim@gmail.com>
2021-03-16 21:41:31 +0900

    Practice auto layout && SnapKit

---
+++ iOS_Swift/CleanSwift/CleanStore/CleanStore/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CleanStore
+//
+//  Created by 김예랑 on 2021/03/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
---
+++ iOS_Swift/CleanSwift/CleanStore/CleanStoreTests/CleanStoreTests.swift
@@ -0,0 +1,33 @@
+//
+//  CleanStoreTests.swift
+//  CleanStoreTests
+//
+//  Created by 김예랑 on 2021/03/13.
+//
+
+import XCTest
+@testable import CleanStore
+
+class CleanStoreTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
